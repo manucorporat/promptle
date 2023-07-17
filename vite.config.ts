b31c2836c43f220a3e3a6e2733a0c8818394c26c
Manu MA <manu.mtza@gmail.com>
2023-07-17 21:55:39 +0200

    😸

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { vanillaExtractPlugin } from "styled-vanilla-extract/vite";
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), vanillaExtractPlugin()],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
